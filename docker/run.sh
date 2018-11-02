900af55117c96d33e7e2c8ac798be95e7242d8a8
Romain GÉRARD <r.gerard@criteo.com>
2018-11-02 18:09:21 +0100

    Docker: Allow to be able to run as non-root

--- docker/run.sh
+++ docker/run.sh
@@ -7,4 +7,4 @@ export JVM_OPTS="${JVM_OPTS}" # i.e -Xmx1024m
 echo "Starting Casspoke"
 echo "JVM_OPTS: $JVM_OPTS"
 
-/sbin/dumb-init /usr/bin/java ${JVM_OPTS} -jar /opt/casspoke/casspoke.jar /etc/casspoke/config.yml
+/sbin/dumb-init /usr/bin/java ${JVM_OPTS} -jar /opt/casspoke/casspoke.jar /opt/casspoke/config.yml
